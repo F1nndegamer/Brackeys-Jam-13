013430a6aa9ab9e3d36e9612dc1162227e7a2a41
icantdo <80340838+icantdo@users.noreply.github.com>
2025-02-16 16:48:25 +0300

    Update Spikes.cs

--- Assets/Script/Spikes.cs
+++ Assets/Script/Spikes.cs
@@ -4,7 +4,7 @@ public class Spikes : Traps
 {
     public override void OnCollisionEnter2D(Collision2D collision)
     {
-        base.OnCollisionEnter2D(collision);
-        Debug.Log(collision.gameObject.name.ToString());
+        base.OnCollisionEnter2D(collision); // call base trap work
+        Debug.Log(collision.gameObject.name.ToString()); // add new func for spike
     }
 }
