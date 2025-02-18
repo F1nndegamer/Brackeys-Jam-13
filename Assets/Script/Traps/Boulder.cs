2f350cdd57b4f0d4e1ced94b2ebc2c83abcd31ff
icantdo <80340838+icantdo@users.noreply.github.com>
2025-02-19 00:52:23 +0300

    update mainscene1

--- Assets/Script/Traps/Boulder.cs
+++ Assets/Script/Traps/Boulder.cs
@@ -7,7 +7,7 @@ public class Boulder : Traps
     public float boulderMove;
     public override void OnCollisionEnter2D(Collision2D collision)
     {
-        if (GetComponent<Rigidbody2D>().linearVelocityX != 0 || GetComponent<Rigidbody2D>().linearVelocityY != 0)
+        if (GetComponent<Rigidbody2D>().linearVelocityX != 0.01f || GetComponent<Rigidbody2D>().linearVelocityY != 0.01f)
         {
             base.OnCollisionEnter2D(collision);
         }
@@ -16,7 +16,12 @@ public class Boulder : Traps
     {
         if (isActive && GameManager.instance.isTakenTreasure)
         {
-            GetComponent<Rigidbody2D>().linearVelocityX = boulderMove;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<Rigidbody2D>().AddForceX(boulderMove, ForceMode2D.Impulse);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         }
     }
 }
