93533430899ef72f65dabfe002fd4c1c8c2b20e6
icantdo <80340838+icantdo@users.noreply.github.com>
2025-02-19 00:26:00 +0300

    implement boulder in MainScene1

--- Assets/Script/Traps/Boulder.cs
+++ Assets/Script/Traps/Boulder.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Boulder : Traps
 {
+    public float boulderMove;
     public override void OnCollisionEnter2D(Collision2D collision)
     {
         if (GetComponent<Rigidbody2D>().linearVelocityX != 0 || GetComponent<Rigidbody2D>().linearVelocityY != 0)
         {
             base.OnCollisionEnter2D(collision);
+        }
+    }
+    private void Update()
+    {
+        if (isActive && GameManager.instance.isTakenTreasure)
+        {
+            GetComponent<Rigidbody2D>().linearVelocityX = boulderMove;
         }
     }
 }
