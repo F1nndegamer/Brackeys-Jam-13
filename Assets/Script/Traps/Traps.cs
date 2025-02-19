74434f74527363a5b81df570635abee88c833a98
icantdo <80340838+icantdo@users.noreply.github.com>
2025-02-19 18:03:07 +0300

    Update Traps.cs

--- Assets/Script/Traps/Traps.cs
+++ Assets/Script/Traps/Traps.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Traps : MonoBehaviour
 {
     public float damage;
-    public bool isActive = true; //when press Deactive button, is false
+    public bool isActive = true; //when press Deactive, is false
     public virtual void OnCollisionEnter2D(Collision2D collision)
     {
         if (isActive && GameManager.instance.isTakenTreasure && collision.gameObject.CompareTag("Player"))
