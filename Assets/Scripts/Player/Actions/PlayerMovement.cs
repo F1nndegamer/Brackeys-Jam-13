45d8bc45d8eb5eefa64e558f536fc929b8858e6f
F1nndegamer <f1nndevgamer@gmail.com>
2025-02-20 15:11:58 +0100

    Revert bugfix and fix the bug

--- Assets/Scripts/Player/Actions/PlayerMovement.cs
+++ Assets/Scripts/Player/Actions/PlayerMovement.cs
@@ -7,13 +7,11 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
     private float moveInput;
-    private int debugY;
 
     [Header("Jump Settings")]
     public float jumpForce = 12f;
     private int jumpCount = 0;
     private bool isGrounded;
-    [SerializeField] private float yaxisSpeedLimit = -15;
 
     [Header("Dash Settings")]
     public float dashSpeed = 20f;
@@ -81,7 +79,6 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
             }
         }
-        debugY = (int)rb.linearVelocityY;
     }
 
     void FixedUpdate()
@@ -106,12 +103,6 @@ public class PlayerMovement : MonoBehaviour
         if (!isDashing)
         {
             rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-        }
-
-        //falling gugfix
-        if (rb.linearVelocityY < yaxisSpeedLimit)
-        {
-            rb.linearVelocityY = yaxisSpeedLimit;
         }
 
         /* --- Footstep SFX Trigger ---
