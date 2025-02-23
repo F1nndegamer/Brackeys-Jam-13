e23d466292447137124a8b024d0f6ce5ab9bcc8c
icantdo <80340838+icantdo@users.noreply.github.com>
2025-02-23 09:10:45 +0300

    temp conveer belt

--- Assets/Scripts/Traps/Moving/ConveerBelt.cs
+++ Assets/Scripts/Traps/Moving/ConveerBelt.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ConveerBelt : MonoBehaviour
 {
     public enum Rotation { Right, Left, Stop }
-    
+
     private Rigidbody2D rb2D;
     [SerializeField] private float speed = 1;
     public Rotation rotation;
@@ -13,53 +13,25 @@ public class ConveerBelt : MonoBehaviour
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        rb2D.bodyType = RigidbodyType2D.Kinematic;
-        
-        // Ensure the conveyor belt has a trigger collider
-        Collider2D col = GetComponent<Collider2D>();
-        if (col != null) col.isTrigger = true;
     }
 
     private void FixedUpdate()
     {
-        if (!activateswitch) return;
-
-        Vector2 movement = Vector2.zero;
-
-        if (rotation == Rotation.Right)
+        if (rotation == Rotation.Left)
         {
-            movement = Vector2.right * speed;
+            Vector2 pos = rb2D.position;
+            rb2D.position += Vector2.right * speed * Time.fixedDeltaTime;
+            rb2D.MovePosition(pos);
         }
-        else if (rotation == Rotation.Left)
+        else if (rotation == Rotation.Right)
         {
-            movement = Vector2.left * speed;
+            Vector2 pos = rb2D.position;
+            rb2D.position += -Vector2.right * speed * Time.fixedDeltaTime;
+            rb2D.MovePosition(pos);
         }
-
-        rb2D.MovePosition(rb2D.position + movement * Time.fixedDeltaTime);
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        Rigidbody2D objRb = other.attachedRigidbody;
-        if (objRb != null)
+        else
         {
-            if (rotation == Rotation.Right)
-            {
-                objRb.linearVelocity = new Vector2(speed, objRb.linearVelocity.y);
-            }
-            else if (rotation == Rotation.Left)
-            {
-                objRb.linearVelocity = new Vector2(-speed, objRb.linearVelocity.y);
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        Rigidbody2D objRb = other.attachedRigidbody;
-        if (objRb != null)
-        {
-            objRb.linearVelocity = new Vector2(0, objRb.linearVelocity.y); // Reset horizontal velocity when leaving the belt
+            //do nothings
         }
     }
 }
